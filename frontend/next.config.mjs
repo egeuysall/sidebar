d8fd7e68556ecf224193972b140a08ae604fd147
Cole Caccamise <colecaccamise@gmail.com>
2024-11-22 17:02:33 -0500

    chore: update next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		domains: ['d2eu2jqkbj4sko.cloudfront.net'],
+	},
+	webpack: (config) => {
+		config.resolve.alias['@'] = path.resolve(__dirname);
+		return config;
 	},
 };
 
