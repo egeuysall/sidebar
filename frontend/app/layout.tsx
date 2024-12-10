e943c953c8f8a91e568db6af93e31b675262c889
Cole Caccamise <colecaccamise@gmail.com>
2024-12-10 18:21:39 -0500

    chore: configure frontend analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,42 +1,50 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
-import { Toaster } from 'sonner';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Toaster } from "sonner";
+import PlausibleProvider from "next-plausible";
+import { CSPostHogProvider } from "./providers";
 
 const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: 'Go Dashboard',
-	description: 'SaaS boilerplate built with Next.js and Go.',
+  title: "Go Dashboard",
+  description: "SaaS boilerplate built with Next.js and Go.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased dark:dark text-typography-weak bg-background`}
-			>
-				<Toaster
-					duration={3000}
-					position='bottom-right'
-				/>
-				<div className='w-full min-h-screen font-sans antialiased flex flex-col justify-center items-center p-6'>
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:dark text-typography-weak bg-background`}
+      >
+        <Toaster duration={3000} position="bottom-right" />
+        <div className="w-full min-h-screen font-sans antialiased flex flex-col justify-center items-center p-6">
+        <PlausibleProvider
+          domain={process.env.NEXT_PUBLIC_APP_URL || ""}
+          trackOutboundLinks={true}
+          taggedEvents={true}
+          trackLocalhost={false}
+        >
+          <CSPostHogProvider>
+            <>{children}</>
+            </CSPostHogProvider>
+          </PlausibleProvider>
+        </div>
+      </body>
+    </html>
+  );
 }
