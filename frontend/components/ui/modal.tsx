d333576f3e8373eec43c6196b5e16aafdc2a6dea
Cole Caccamise <colecaccamise@gmail.com>
2024-12-10 18:21:16 -0500

    feat: update modal design

--- frontend/components/ui/modal.tsx
+++ frontend/components/ui/modal.tsx
@@ -1,36 +1,42 @@
-import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
-import { useState } from "react";
+import { Dialog, DialogPanel, DialogBackdrop } from "@headlessui/react";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import Button from "@/components/ui/button";
 
-export default function Modal() {
-  let [isOpen, setIsOpen] = useState(false);
-
+export default function Modal({
+  open = false,
+  children,
+  onClose = () => {},
+  title,
+}: {
+  open?: boolean;
+  children?: React.ReactNode;
+  onClose?: (value: boolean) => void;
+  title?: string;
+}) {
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open dialog</button>
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50"
-      >
+      <Dialog open={open} onClose={onClose} className="relative z-50">
+        <DialogBackdrop className="fixed inset-0 bg-black/40" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <Description>
-              This will permanently deactivate your account
-            </Description>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
-            <div className="flex gap-4">
-              <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button onClick={() => setIsOpen(false)}>Deactivate</button>
-            </div>
+          <DialogPanel className="max-w-lg space-y-4 border border-stroke-weak bg-background p-8 rounded-md">
+            {title ? (
+              <div className="flex justify-between">
+                <h3 className="text-lg text-typography-strong font-bold">
+                  {title}
+                </h3>
+                <Button
+                  handleClick={() => onClose(false)}
+                  className="hover:opacity-80 transition-effect"
+                  variant="unstyled"
+                >
+                  <Cross1Icon />
+                </Button>
+              </div>
+            ) : (
+              <></>
+            )}
+
+            {children}
           </DialogPanel>
         </div>
       </Dialog>
