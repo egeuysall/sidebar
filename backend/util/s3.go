2c8e77051a479e353368528a5addcae02dc94b23
Cole Caccamise <colecaccamise@gmail.com>
2024-12-11 22:03:23 -0500

    feat: add s3 golang util

--- backend/util/s3.go
+++ backend/util/s3.go
@@ -1,45 +1,86 @@
 package util
 
-// func UploadFileToS3(file []byte) (string, string, error) {
-// 	cfg, err := config.LoadDefaultConfig(context.TODO())
-// 	if err != nil {
-// 		log.Printf("error: %v", err)
-// 		return "", "", err
-// 	}
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
+	"github.com/h2non/filetype"
+	"github.com/nfnt/resize"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+)
 
-// 	client := s3.NewFromConfig(cfg)
+func UploadFileToS3(file []byte) (string, string, error) {
+	// aws config
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(os.Getenv("AWS_REGION")),
+	)
+	if err != nil {
+		return "", "", fmt.Errorf("aws config error: %v", err)
+	}
 
-// 	uuid := uuid.New().String()
+	// generate unique filename
+	key := uuid.New().String()
+	fileType, err := filetype.MatchReader(bytes.NewReader(file))
+	if err != nil {
+		return "", "", fmt.Errorf("file type detection error: %v", err)
+	}
+	filename := fmt.Sprintf("%s.%s", key, fileType.Extension)
 
-// 	fileType, err := filetype.MatchReader(bytes.NewReader(file))
-// 	if err != nil {
-// 		return "", "", err
-// 	}
+	// create thumbnail
+	img, format, err := image.Decode(bytes.NewReader(file))
+	if err != nil {
+		return "", "", fmt.Errorf("image decode error: %v", err)
+	}
 
-// 	filename := fmt.Sprintf("%s.%s", uuid, fileType.Extension)
+	// use buffered writer for thumbnail
+	var thumbnailBuf bytes.Buffer
+	thumbnail := resize.Resize(160, 0, img, resize.Lanczos3)
 
-// 	// Decoding gives you an Image.
-// 	// If you have an io.Reader already, you can give that to Decode
-// 	// without reading it into a []byte.
-// 	image, _, err := image.Decode(bytes.NewReader(data))
-// 	// check err
+	// encode based on original format
+	switch format {
+	case "jpeg", "jpg":
+		err = jpeg.Encode(&thumbnailBuf, thumbnail, nil)
+	case "png":
+		err = png.Encode(&thumbnailBuf, thumbnail)
+	default:
+		return "", "", fmt.Errorf("unsupported image format: %s", format)
+	}
+	if err != nil {
+		return "", "", fmt.Errorf("thumbnail encode error: %v", err)
+	}
 
-// 	newImage := resize.Resize(160, 0, original_image, resize.Lanczos3)
+	// upload original and thumbnail
+	client := s3.NewFromConfig(cfg)
+	uploader := manager.NewUploader(client)
 
-// 	// Encode uses a Writer, use a Buffer if you need the raw []byte
-// 	err = jpeg.Encode(someWriter, newImage, nil)
-// 	// check err
+	// upload original
+	uploadOutput, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
+		Key:    aws.String(filename),
+		Body:   bytes.NewReader(file),
+	})
+	if err != nil {
+		return "", "", fmt.Errorf("original file upload error: %v", err)
+	}
 
-// 	uploader := manager.NewUploader(client)
-// 	uploadOutput, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-// 		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
-// 		Key:    aws.String(filename),
-// 		Body:   bytes.NewReader(file),
-// 	})
+	// upload thumbnail with _thumb suffix
+	thumbnailKey := fmt.Sprintf("%s_thumb.%s", key, fileType.Extension)
+	_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
+		Key:    aws.String(thumbnailKey),
+		Body:   bytes.NewReader(thumbnailBuf.Bytes()),
+	})
+	if err != nil {
+		return "", "", fmt.Errorf("thumbnail upload error: %v", err)
+	}
 
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-
-// 	return filename, uploadOutput.Location, nil
-// }
+	return filename, uploadOutput.Location, nil
+}
