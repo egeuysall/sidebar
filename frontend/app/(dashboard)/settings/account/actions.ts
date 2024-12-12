c0cb1c2bdbdcb74dc43745a44fb7089dc98bd6c7
Cole Caccamise <colecaccamise@gmail.com>
2024-12-11 22:06:36 -0500

    chore: add verify password action

--- frontend/app/(dashboard)/settings/account/actions.ts
+++ frontend/app/(dashboard)/settings/account/actions.ts
@@ -4,6 +4,7 @@ import { User } from "@/types";
 
 import axios from "axios";
 import { cookies } from "next/headers";
+import { parseNextCookie } from "@/lib/cookie";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -48,12 +49,19 @@ export async function updateUserEmail({
 }) {
   if (!user || !email) return;
 
+  const cookieStore = cookies();
+
   const response = await axios
     .patch(
       `${apiUrl}/users/email`,
       { email },
       {
-        headers: { Cookie: `auth-token=${cookies().get("auth-token")?.value}` },
+        headers: {
+          Cookie: cookieStore
+            .getAll()
+            .map((cookie) => `${cookie.name}=${cookie.value}`)
+            .join("; "),
+        },
       },
     )
     .then((res) => {
@@ -104,4 +112,33 @@ export async function uploadAvatar({
     });
 
   return response;
+}
+
+export async function verifyPassword({ password }: { password: string }) {
+  await axios
+    .post(
+      `${apiUrl}/auth/verify-password`,
+      {
+        password,
+      },
+      {
+        headers: { Cookie: `auth-token=${cookies().get("auth-token")?.value}` },
+      },
+    )
+    .then(async (res) => {
+      const setCookieHeader: string | string[] | undefined =
+        res.headers["set-cookie"];
+      if (setCookieHeader) {
+        const cookieStore = cookies();
+
+        setCookieHeader.forEach((c) => {
+          const parsed = parseNextCookie(c);
+          cookieStore.set(parsed.name, parsed.value, parsed.options);
+        });
+      }
+      return res.data;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
