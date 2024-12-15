a1015c49f8ec4d9998e944978c564b68b5ed6c4b
Cole Caccamise <colecaccamise@gmail.com>
2024-12-15 13:22:38 -0500

    feat: redesign auth layout

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -1,11 +1,25 @@
-import {Suspense} from "react";
+import { Suspense } from 'react';
+import Link from 'next/link';
+import Logo from '@/components/ui/logo';
 
 export default function AuthLayout({
-	children,
+	children
 }: {
 	children: React.ReactNode;
 }) {
-	return <Suspense>
-		<div className='w-full max-w-md'>{children}</div>
-	</Suspense>
+	return (
+		<Suspense>
+			<div className='flex flex-col h-full justify-between items-center flex-grow py-8'>
+				<Logo />
+				<div className='w-full max-w-md'>{children}</div>
+				<div>
+					<p className='text-sm'>
+						By proceeding you acknowledge that you have read, understood and
+						agree to our <Link href='/legal/terms'>Terms of Service</Link> and{' '}
+						<Link href='/legal/privacy'>Privacy Policy</Link>.
+					</p>
+				</div>
+			</div>
+		</Suspense>
+	);
 }
