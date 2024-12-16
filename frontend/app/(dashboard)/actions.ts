ae25ce041ff76374d3ba0d364edc7aaeba86b84c
Cole Caccamise <colecaccamise@gmail.com>
2024-12-16 07:13:30 -0500

    chore: add restore action

--- frontend/app/(dashboard)/actions.ts
+++ frontend/app/(dashboard)/actions.ts
@@ -1,33 +1,55 @@
-'use server';
+"use server";
 
-import axios from 'axios';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import axios from "axios";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function handleLogout() {
-	'use server';
+  "use server";
 
-	try {
-		const response = await axios.post(
-			`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`,
-			{},
-			{
-				withCredentials: true,
-			}
-		);
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/logout`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
 
-		const setCookie = response.headers['set-cookie'];
-		console.log('Set-Cookie:', setCookie);
+    const setCookie = response.headers["set-cookie"];
+    console.log("Set-Cookie:", setCookie);
 
-		if (setCookie) {
-			setCookie.forEach((cookie) => {
-				const [name] = cookie.split('=');
-				cookies().delete(name);
-			});
-		}
+    if (setCookie) {
+      setCookie.forEach((cookie) => {
+        const [name] = cookie.split("=");
+        cookies().delete(name);
+      });
+    }
 
-		redirect('/auth/login');
-	} catch {
-		redirect('/auth/login');
-	}
+    redirect("/auth/login");
+  } catch {
+    redirect("/auth/login");
+  }
+}
+
+export async function handleRestoreUser() {
+  "use server";
+
+  return axios
+    .patch(
+      `${process.env.NEXT_PUBLIC_API_URL}/users/restore`,
+      {},
+      {
+        headers: {
+          Cookie: `auth-token=${cookies().get("auth-token")?.value}`,
+        },
+        withCredentials: true,
+      }
+    )
+    .then((res: any) => {
+      return res.data;
+    })
+    .catch((err: any) => {
+      return err.response.data;
+    });
 }
