3a1e870bba54e3e7be6a8dc6543c4e1ec4759b20
Cole Caccamise <colecaccamise@gmail.com>
2024-12-16 07:18:37 -0500

    chore: cleanup login page

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -9,118 +9,114 @@ import { ApiError } from '@/types';
 import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { getErrorMessage } from '@/messages';
 
 export default function LoginPage() {
-	const searchParams = useSearchParams();
-	const router = useRouter();
-	const [isLoading, setIsLoading] = useState(false);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-	useEffect(() => {
-		const error = searchParams.get('error');
-		if (error === 'confirm-email-token-invalid') {
-			toast({
-				message:
-					'The confirmation link was invalid or has expired. Please login to request a new one.',
-				mode: 'error'
-			});
-		}
-	}, [searchParams]);
+  useEffect(() => {
+    const error = searchParams.get('error');
+    if (error === 'confirm-email-token-invalid') {
+      toast({
+        message:
+          'The confirmation link was invalid or has expired. Please login to request a new one.',
+        mode: 'error',
+      });
+    }
+  }, [searchParams]);
 
-	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-		setIsLoading(true);
+    setIsLoading(true);
 
-		try {
-			const response = await axios.post(
-				`${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-				{ email, password },
-				{
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					withCredentials: false
-				}
-			);
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+        { email, password },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+        },
+      );
 
-			if (response.status === 200) {
-				toast({
-					message: 'Logged in successfully',
-					mode: 'success'
-				});
-				router.push('/dashboard');
-			}
-		} catch (error) {
-			if (axios.isAxiosError(error) && error.response) {
-				const apiError = error.response.data as ApiError;
-				toast({
-					message: 'Invalid credentials',
-					mode: 'error'
-				});
-			} else {
-				toast({
-					message: 'An unexpected error occurred',
-					mode: 'error'
-				});
-			}
-		}
+      if (response.status === 200) {
+        toast({
+          message: 'Logged in successfully',
+          mode: 'success',
+        });
+        router.push('/dashboard');
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const apiError = error.response.data as ApiError;
+        toast({
+          message: getErrorMessage(apiError.code),
+          mode: 'error',
+        });
+      } else {
+        toast({
+          message: getErrorMessage('internal_server_error'),
+          mode: 'error',
+        });
+      }
+    }
 
-		setIsLoading(false);
-	}
+    setIsLoading(false);
+  }
 
-	return (
-		<div className='flex flex-col gap-4'>
-			<h1>Log in to your account</h1>
-			<form
-				onSubmit={handleSubmit}
-				className='flex flex-col gap-6'
-			>
-				<Input
-					label='Email address'
-					type='email'
-					name='email'
-					placeholder='name@company.com'
-					required
-					value={email}
-					handleChange={(e) => setEmail(e.target.value)}
-				/>
-				<Input
-					label='Password'
-					type='password'
-					name='password'
-					placeholder='Password'
-					required
-					value={password}
-					handleChange={(e) => setPassword(e.target.value)}
-					hint={
-						<Link
-							className='text-xs text-typography-weak'
-							href='/auth/forgot-password'
-						>
-							Forgot password?
-						</Link>
-					}
-				/>
-				<Button
-					className='w-full'
-					type='submit'
-					disabled={isLoading || !email || !password}
-					loading={isLoading}
-				>
-					Continue with email
-				</Button>
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>Log in to your account</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+        <Input
+          label="Email address"
+          type="email"
+          name="email"
+          placeholder="name@company.com"
+          required
+          value={email}
+          handleChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={password}
+          handleChange={(e) => setPassword(e.target.value)}
+          hint={
+            <Link
+              className="text-xs text-typography-weak"
+              href="/auth/forgot-password"
+              tabIndex={-1}
+            >
+              Forgot password?
+            </Link>
+          }
+        />
+        <Button
+          className="w-full"
+          type="submit"
+          disabled={isLoading || !email || !password}
+          loading={isLoading}
+        >
+          Continue with email
+        </Button>
 
-				<div className='text-center'>
-					<Link
-						className='no-underline'
-						href='/auth/signup'
-					>
-						or sign up instead
-					</Link>
-				</div>
-			</form>
-		</div>
-	);
+        <div className="text-center">
+          <Link className="no-underline" href="/auth/signup">
+            or sign up instead
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
