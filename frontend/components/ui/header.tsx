4c7787f33079ac058a2dbc38fc180326cdba69d6
Cole Caccamise <colecaccamise@gmail.com>
2024-12-16 07:07:18 -0500

    fix: remove underline on header

--- frontend/components/ui/header.tsx
+++ frontend/components/ui/header.tsx
@@ -1,17 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Header({ children }: { children: React.ReactNode }) {
-	return (
-		<header className='w-full'>
-			<nav className='flex justify-between items-center'>
-				<Link
-					className='text-lg font-medium text-typography-strong'
-					href='/dashboard'
-				>
-					Dashboard
-				</Link>
-				<div>{children}</div>
-			</nav>
-		</header>
-	);
+  return (
+    <header className="w-full">
+      <nav className="flex items-center justify-between">
+        <Link
+          className="text-lg font-medium text-typography-strong no-underline"
+          href="/dashboard"
+        >
+          Dashboard
+        </Link>
+        <div>{children}</div>
+      </nav>
+    </header>
+  );
 }
