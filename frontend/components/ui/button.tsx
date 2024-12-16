3c725314872dd966724131583e1d75c615bdfb56
Cole Caccamise <colecaccamise@gmail.com>
2024-12-16 07:12:36 -0500

    feat: add destructive button

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -1,95 +1,99 @@
-import Spinner from './spinner';
-import Link from 'next/link';
+import Spinner from "./spinner";
+import Link from "next/link";
 
 export default function Button({
-	className,
-	children,
-	type = 'button',
-	variant,
-	loading = false,
-	disabled,
-	handleClick,
-	href,
+  className,
+  children,
+  type = "button",
+  variant,
+  loading = false,
+  disabled,
+  handleClick,
+  href,
 }: {
-	className?: string;
-	color?: string;
-	children: React.ReactNode;
-	type?: 'button' | 'submit' | 'reset';
-	variant?: string;
-	loading?: boolean;
-	disabled?: boolean;
-	handleClick?: () => void;
-	href?: string;
+  className?: string;
+  color?: string;
+  children: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  variant?: string;
+  loading?: boolean;
+  disabled?: boolean;
+  handleClick?: () => void;
+  href?: string;
 }) {
-	// const backgroundColor = color ? `bg-${color}` : undefined;
-	// TODO: create variants for destructive, outline, ghost, link, icon, text w/ icon
-	// reference: https://ui.shadcn.com/docs/components/button
+  // const backgroundColor = color ? `bg-${color}` : undefined;
+  // TODO: create variants for destructive, outline, ghost, link, icon, text w/ icon
+  // reference: https://ui.shadcn.com/docs/components/button
 
-	if (variant === 'link') {
-		return (
-			<Link
-				className={`${className} text-typography-strong hover:text-typography-strong`}
-				onClick={handleClick}
-				href={href || ''}
-			>
-				{children}
-			</Link>
-		);
-	}
+  if (variant === "link") {
+    return (
+      <Link
+        className={`${className} text-typography-strong hover:text-typography-strong`}
+        onClick={handleClick}
+        href={href || ""}
+      >
+        {children}
+      </Link>
+    );
+  }
 
-	if (variant === 'destructive') {
-		return (
-			<button
-				type={type}
-				disabled={loading || disabled}
-				className={`btn btn-destructive whitespace-nowrap ${
-					(loading || disabled) && 'btn-disabled'
-				}`}
-				onClick={handleClick}
-			>
-				{loading && <Spinner />}
+  if (variant === "destructive") {
+    return (
+      <button
+        type={type}
+        disabled={loading || disabled}
+        className={`btn btn-destructive whitespace-nowrap ${className} ${
+          (loading || disabled) && "btn-disabled"
+        }`}
+        onClick={handleClick}
+      >
+        {loading && <Spinner />}
 
-				{children}
-			</button>
-		);
-	}
+        {children}
+      </button>
+    );
+  }
 
-	if (variant === 'unstyled') {
-		return (
-			<button
-				type={type}
-				disabled={loading || disabled}
-				className={`${className} whitespace-nowrap ${
-					(loading || disabled) && 'btn-disabled'
-				} group flex items-center`}
-				onClick={handleClick}
-			>
-				<span className={loading ? 'pr-2' : ''}>{loading && <Spinner />}</span>
+  if (variant === "unstyled") {
+    return (
+      <button
+        type={type}
+        disabled={loading || disabled}
+        className={`${className} whitespace-nowrap ${
+          (loading || disabled) && "btn-disabled"
+        } group flex items-center`}
+        onClick={handleClick}
+      >
+        <span className={loading ? "pr-2" : ""}>{loading && <Spinner />}</span>
 
-				{children}
-			</button>
-		);
-	}
+        {children}
+      </button>
+    );
+  }
 
-	const buttonClasses =
-		variant === 'secondary' ? 'btn btn-secondary' : 'btn btn-brand';
+  const buttonClasses =
+    variant === "secondary"
+      ? "btn btn-secondary"
+      : variant === "destructive"
+        ? "btn btn-destructive"
+        : "btn btn-brand";
 
-	return (
-		<button
-			type={type}
-			disabled={loading || disabled}
-			className={`${className} ${buttonClasses} whitespace-nowrap ${
-				(loading || disabled) && 'btn-disabled'
-			} group flex items-center`}
-			onClick={handleClick}
-		>
-			<span className={loading ? 'pr-2' : ''}>
-				{loading && (
-					<Spinner variant={variant === 'brand' ? 'light' : 'dark'} />
-				)}
-			</span>
+  return (
+    <button
+      type={type}
+      disabled={loading || disabled}
+      className={`${className} ${buttonClasses} whitespace-nowrap ${
+        (loading || disabled) && "btn-disabled"
+      } group flex items-center`}
+      onClick={handleClick}
+    >
+      <span className={loading ? "pr-2" : ""}>
+        {loading && (
+          <Spinner variant={variant === "brand" ? "light" : "dark"} />
+        )}
+      </span>
 
-			{children}
-		</button>
-	);
+      {children}
+    </button>
+  );
 }
